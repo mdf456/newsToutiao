20502b3047e543ef12a18286da6cbb661b6eab0b
Vico_pan <vico_pan@foxmail.com>
2017-07-10 18:03:15 +0800

    second

--- app/src/main/java/com/vico/newstoutiao/activitys/MainActivity.java
+++ app/src/main/java/com/vico/newstoutiao/activitys/MainActivity.java
@@ -2,14 +2,17 @@ package com.vico.newstoutiao.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.vico.newstoutiao.R;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("test", "onCreate: ");
     }
+
 }
