76f1ebc0f86ccee40060b48c86a3494de75cee3f
Vico_pan <vico_pan@foxmail.com>
2017-07-10 16:39:38 +0800

    second

--- app/src/main/java/com/vico/newstoutiao/activitys/MainActivity.java
+++ app/src/main/java/com/vico/newstoutiao/activitys/MainActivity.java
@@ -1,7 +1,9 @@
-package com.vico.newstoutiao;
+package com.vico.newstoutiao.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vico.newstoutiao.R;
 
 public class MainActivity extends AppCompatActivity {
 
