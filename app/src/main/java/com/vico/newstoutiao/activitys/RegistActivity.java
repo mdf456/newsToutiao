bd33e766ad9fbdb980d154a3a12d2490bca92779
Vico_pan <vico_pan@foxmail.com>
2017-07-11 11:22:52 +0800

    登录注册布局

---
+++ app/src/main/java/com/vico/newstoutiao/activitys/RegistActivity.java
@@ -0,0 +1,19 @@
+package com.vico.newstoutiao.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.vico.newstoutiao.R;
+
+/**
+ * Created by ${Vico} on 2017/7/11.
+ */
+
+public class RegistActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+}
---
+++ app/src/main/java/com/vico/newstoutiao/adapters/Myadapter.java
@@ -0,0 +1,8 @@
+package com.vico.newstoutiao.adapters;
+
+/**
+ * Created by ${Vico} on 2017/7/11.
+ */
+
+public class Myadapter {
+}
