63a4f40eda0f532c0070fae3c9a37490db8c0c5f
suguangmin <1016417477@qq.com>
2017-07-12 14:52:44 +0800

    主页布局

---
+++ app/src/main/java/com/vico/newstoutiao/SlidingMenus/SlidingMenu.java
@@ -0,0 +1,9 @@
+package com.vico.newstoutiao.SlidingMenus;
+
+/**
+ * Created by Administrator on 2017/7/12 0012.
+ */
+
+//public class SlidingMenu initSlidingMeu(Activity activity){
+//        SlidingMenu localSlidingMenu = new SlidingMenu(activity);
+//        }
